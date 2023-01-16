4370bd522413acc7b46a1e55197f72dd77baf347
Alexander Garzo <87275990+garzo94@users.noreply.github.com>
2023-01-16 15:47:36 -0600

    Update App.js
    Removing styes object

--- flights/App.js
+++ flights/App.js
@@ -3,18 +3,11 @@ import { StyleSheet, Text, View } from "react-native";
 import SignUp from "./screens/SignUp";
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <SignUp />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-   backgroundColor: "#fff",
-    flex: 1,
-    marginTop: 40,
-    width: "100%",
-  },
-});
+
