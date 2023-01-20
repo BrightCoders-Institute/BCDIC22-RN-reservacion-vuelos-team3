3693fc196dea10ee6b68efbe4134654874b8d81b
Arturo Alejandro China <arthurchinaa@gmail.com>
2023-01-20 15:35:29 -0600

    Update styles.js
    Styles alphabetically sorted

--- flights/styles/styles.js
+++ flights/styles/styles.js
@@ -105,4 +105,45 @@ export const styles = StyleSheet.create({
   },
   bottomText: { marginTop: 20, color: "gray", alignSelf: "center" },
   linkLogin: { color: "#4285f4", textDecorationLine: "underline" },
+  centeredView: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 20,
+    elevation: 5,
+    margin: 20,
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+  button: {
+    borderRadius: 20,
+    elevation: 2,
+    padding: 10, 
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
 });
